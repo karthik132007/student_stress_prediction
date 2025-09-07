5ff98566513b35eb67756f2ac299e1cd8922544e
Karthikeya <kamalkarthikeyakumar@gmail.com>
2025-09-07 21:44:20 +0530

    questions

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from datetime import datetime
 # Step 1: Start
 st.title("🧠 Mental Health Self-Assessment")
 start_test = st.button("Start Test")
-
+answers=[]
 if start_test:
     with st.form("mental_health_form"):
         st.header("👤 Personal Info")
         student_name = st.text_input("Enter your name")
-        dob = st.date_input("Enter your Date of Birth", min_value=datetime(1900, 1, 1), max_value=datetime.today())
+        age = st.number_input("Enter your age",)
 
         st.header("📝 Questionnaire")
-        self_esteem = st.slider("**1. Self Esteem**", 0, 30)
-
-        bullying = st.radio(
-            "**2. Bullying**",
+        
+        st.markdown("<h4 style='font-size:20px;'>1.👉 On a scale of 0 to 30, how confident do you feel about yourself and your abilities?</h4>", unsafe_allow_html=True)
+        q1 = st.slider("**Self Esteem**", 0, 30)
+        st.markdown("---")
+        
+        st.markdown("<h4 style='font-size:20px;'>2.👉 In the past few months, how often have you experienced or felt affected by bullying?</h4>", unsafe_allow_html=True)
+        q2 = st.radio(
+            "**Bullying**",
             [
                 "Never",
                 "Rarely",
@@ -26,9 +30,10 @@ if start_test:
                 "Constantly"
             ]
         )
-
-        sleep_quality = st.radio(
-            "**3. Sleep Quality**",
+        st.markdown("---")
+        st.markdown("<h4 style='font-size:20px;'>3.👉 How would you rate your sleep quality recently?</h4>", unsafe_allow_html=True)
+        q3 = st.radio(
+            "**Sleep Quality**",
             [
                 "Very Poor",
                 "Poor",
@@ -38,9 +43,10 @@ if start_test:
                 "Excellent"
             ]
         )
-
-        career_concerns = st.radio(
-            "**4. Future Career Concerns**",
+        st.markdown("---")
+        st.markdown("<h4 style='font-size:20px;'>4. 👉 To what extent are you worried about your future career?</h4>", unsafe_allow_html=True)
+        q4 = st.radio(
+            "**Future Career Concerns**",
             [
                 "Not at all concerned",
                 "Slightly concerned",
@@ -50,13 +56,19 @@ if start_test:
                 "Extremely concerned"
             ]
         )
+        st.markdown("---")
+        
+        st.markdown("<h4 style='font-size:20px;'>5 .👉 On a scale of 0 to 20, how often have you experienced symptoms of anxiety such as nervousness, worry, or restlessness?</h4>", unsafe_allow_html=True)
+        q5= st.slider("**Anxiety Level**", 0, 20)
+        st.markdown("---")
+        st.markdown("<h4 style='font-size:20px;'>6.👉 On a scale of 0 to 30, how often have you felt down, hopeless, or lost interest in daily activities?</h4>", unsafe_allow_html=True)
+        q6 = st.slider("**6. Depression**", 0, 30)
+        st.markdown("---")
+        
 
-        anxiety = st.slider("**5. Anxiety Level**", 0, 21)
-
-        depression = st.slider("**6. Depression**", 0, 27)
-
-        academic = st.radio(
-            "**7. Academic Performance**",
+        st.markdown("<h4 style='font-size:20px;'>7.👉 How would you rate your academic performance currently?</h4>", unsafe_allow_html=True)
+        q7 = st.radio(
+            "**Academic Performance**",
             [
                 "Very Poor",
                 "Poor",
@@ -66,9 +78,10 @@ if start_test:
                 "Excellent"
             ]
         )
-
-        headache = st.radio(
-            "**8. Headache**",
+        st.markdown("---")
+        st.markdown("<h4 style='font-size:20px;'>8.👉 How often do you experience headaches?</h4>", unsafe_allow_html=True)
+        q8 = st.radio(
+            "**Headache**",
             [
                 "Never",
                 "Rarely",
@@ -78,9 +91,10 @@ if start_test:
                 "Very Frequently"
             ]
         )
-
-        safety = st.radio(
-            "**9. Safety**",
+        st.markdown("---")
+        st.markdown("<h4 style='font-size:20px;'>9.👉 How safe do you feel in your daily environment (home, college, outside)?</h4>", unsafe_allow_html=True)
+        q9 = st.radio(
+            "**Safety**",
             [
                 "Not Safe at All",
                 "Rarely Safe",
@@ -90,10 +104,10 @@ if start_test:
                 "Very Safe"
             ]
         )
-
-
-        basic_needs = st.radio(
-            "**10. Basic Needs**",
+        st.markdown("---")
+        st.markdown("<h4 style='font-size:20px;'>10.👉 To what extent are your basic needs (food, shelter, financial support) being met?</h4>", unsafe_allow_html=True)
+        q10 = st.radio(
+            "**Basic Needs**",
             [
                 "Not Met at All",
                 "Rarely Met",
@@ -108,11 +122,12 @@ if start_test:
 
     if submitted:
         # Calculate age
-        today = datetime.today()
-        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+        st.success("Submitted Successfully")
+        st.balloons()
 
         # Collect responses
-        responses = np.array([
-            self_esteem, bullying, sleep_quality, career_concerns,
-            anxiety, depression, academic, headache, safety, basic_needs
+        answers = np.array([
+            q1,q2,q3,q4,q5,q6,q7,q8,q9,q10
         ])
+        
+print(answers)

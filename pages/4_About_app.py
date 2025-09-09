9842b5fc6fc6c2eb89cd8621f7aaa0209402dc9d
Karthikeya <kamalkarthikeyakumar@gmail.com>
2025-09-09 16:07:52 +0530

    about us app

---
+++ pages/4_About_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.header("About App")
+st.markdown("---")
+
+st.write("This webapp is hosted on Streamlit")
+
+st.markdown('### Requirements :')
+st.markdown("""
+- numpy  
+- pandas  
+- scikit-learn  
+- streamlit  
+- matplotlib  
+- seaborn  
+- plotly  
+- gspread  
+- google-auth  
+- FPDF  
+- google.generativeai  
+""")
